baad10e5f0e1c5d2aa0de2d9090f5eaa09ce331e
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-10-15 23:17:18 -0500

    Nodal window for login built.  Now working on js and firebase login

---
+++ javascripts/main.js
@@ -0,0 +1,24 @@
+requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'lodash': '../lib/bower_components/lodash/lodash.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../lib/bower_components/q/q'
+  },
+  shim: {
+    'bootstrap': ['jquery'],
+    'firebase': {
+            exports: 'Firebase'
+           }
+  }
+});
+
+requirejs(
+  ["jquery", "hbs", "bootstrap", "lodash", "q","getLogin"],
+  function($, Handlebars, bootstrap, _, q, getLogin) {
+
+
+
+  });
